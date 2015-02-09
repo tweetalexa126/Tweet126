27f42801a662a9a59772a37adb0ddfc6f8c308d8
Sanjoy Ghosh <sghosh@hytrust.com>
2015-02-08 22:57:32 -0800

    Start reading stuff

--- src/main/java/com/lab126/alexa/TweetRadioMain.java
+++ src/main/java/com/lab126/alexa/TweetRadioMain.java
@@ -2,38 +2,49 @@ package com.lab126.alexa;
 
 import java.util.Map;
 
+import twitter4j.IDs;
+import twitter4j.PagableResponseList;
 import twitter4j.RateLimitStatus;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.auth.OAuth2Token;
+import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class TweetRadioMain {
 
-	private static final String		BROWSER_CONSUMER_KEY = "OZW4OY9esxNcijm22G5yI2XxC";
-	private static final String		BROWSER_CONSUMER_SECRET = "ZOHWyN95vsG7WTjJbRLYoL3Q3ooM3Uq5WvxweWGke6D7xqr2Pd";
+	private static final String		BROWSER_CONSUMER_KEY = "ie7dINtXBOzDYESMKimM6Ee0s";
+	private static final String		BROWSER_CONSUMER_SECRET = "XMZWUJyOSn6hLQiBsQAF0qdg1HESH4P9PQxeUaPteKQcxZUcZA";
+	private static final String 	ACCESS_TOKEN = "26122273-aWRUdzbQW3YL4KwYOtrJ3UMSVBe3WeblBe9zCbpEq";
+	private static final String		ACCESS_TOKEN_SECRET = "JCuWEysi6D1AtZBX4z0h82iq6BB9ITUFw6cDdPNNJw3K4";
 	
 	
 	public static void main(String[] args) {
 		ConfigurationBuilder builder = new ConfigurationBuilder();
-		builder.setApplicationOnlyAuthEnabled(true);
-		
+		builder.setDebugEnabled(true).
+		setOAuthConsumerKey(BROWSER_CONSUMER_KEY).
+		setOAuthConsumerSecret(BROWSER_CONSUMER_SECRET).
+		setOAuthAccessToken(ACCESS_TOKEN).
+		setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
 		Twitter twitter = new TwitterFactory(builder.build()).getInstance();
 		
         try {
-    		twitter.setOAuthConsumer(BROWSER_CONSUMER_KEY, BROWSER_CONSUMER_SECRET);
-            OAuth2Token token = twitter.getOAuth2Token();
-
             Map<String, RateLimitStatus> rateLimitStatus = twitter.getRateLimitStatus("search");
             RateLimitStatus searchTweetsRateLimit = rateLimitStatus.get("/search/tweets");
     		System.out.println(searchTweetsRateLimit);
 
+    		User user = twitter.showUser("tapasroy");
+    		System.out.println(user);
+    		System.out.println("SSSSS");
+    		PagableResponseList<User> ids = twitter.getFriendsList(user.getId(), -1);
+    		System.out.println(ids);
+    		for (User friend : ids) {
+    			System.out.println(friend);
+    		}
 //            twitter.getAccountSettings();
         } 
         catch (TwitterException e) {
         	e.printStackTrace();
         }
-		System.out.println(twitter);
 	}
 }
